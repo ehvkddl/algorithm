815fd5aa00b90b9e107fb98a8eb50aba01b26f77
Kim Do Hee <ehvkddl@naver.com>
2020-04-10 03:47:29 +0900

    algorithm_피보나치

---
+++ algorithm1_피보나치/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args){
+        int sum = 0;
+        int arr[] = new int[20];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자 입력 (0 <= n <= 20) : ");
+        int num = scanner.nextInt();
+
+        for(int i = 1; i < num; i++ ){
+            arr[i+1] = arr[i-1] + arr[i];
+        }
+
+        System.out.println(arr[num]);
+
+    }
+}
